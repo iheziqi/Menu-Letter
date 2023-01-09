5671aecb8574e3c8dd53ab088cf78f7cb19a3c73
Ziqi He <heziqi4399@gmail.com>
2023-01-09 11:30:06 +0100

    fixed automated shell script and main.js

--- send_menu_letter.sh
+++ send_menu_letter.sh
@@ -1,5 +1,6 @@
-#!usr/bin/zsh
+#!/usr/bin/zsh
 
-node /code/mensa_scraping/src/main.js
+/root/.nvm/versions/node/v18.13.0/bin/node /code/mensa_scraping/src/main.js
 now=$(date)
 echo "Getting dish plans on $now, done!"
+echo "----------------------------------"
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { emailHTML } from './emailTemplate.js';
 import { sendEmail } from './sendEmail.js';
 
 // all users array
-// receivers = ['heziqi4399@gmail.com', 'renshuang1993@gmail.com', '942851168@qq.com', 'yqli0916@gmail.com']
-const receivers = ['ziqi.he@fau.de', 'heziqi4399@gmail.com', 'yqli0916@gmail.com']
+const receivers = ['heziqi4399@gmail.com', 'renshuang1993@gmail.com', '942851168@qq.com', 'yqli0916@gmail.com']
+// const receivers = ['ziqi.he@fau.de', 'heziqi4399@gmail.com', 'yqli0916@gmail.com']
 
 // send email to every user;
 receivers.forEach((receiver) => {
